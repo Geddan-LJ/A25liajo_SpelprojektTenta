25cac3c55dea00fc256775e19363e33f6ce1257f
Geddan-LJ <a25liajo@student.his.se>
2025-11-21 10:09:39 +0100

    Extra och missade punkter
    Jag har med grafik som inte är default i unity, screenspace ui (dialogue och playerhealth), playerhealth plus att scenen startar om ifall man dör, UI knapp (continue dialogue knappen), Fotboll spelet beter sig som en utmaning spelaren ställs inför

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,38 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private List<Image> healthImages;
+    
+    private int maxPlayerHealth;
+    private int playerHealth = 3;
+
+    private void Start()
     {
-        
+        for (int i = 0; i < healthImages.Count; i++)
+        {
+            healthImages[i].gameObject.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if (other.gameObject.CompareTag("Harmfull"))
+        {
+            playerHealth--;
+
+            if (playerHealth > 0)
+            {
+                healthImages[playerHealth].gameObject.SetActive(false);
+            }
+            else if (playerHealth == 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
     }
 }
