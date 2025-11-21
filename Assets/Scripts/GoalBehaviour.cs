c70348dd4100d19182b7daf4404df1dbe047a939
Geddan-LJ <a25liajo@student.his.se>
2025-11-21 09:57:39 +0100

    FotbollsMål Syfte
    Efter 3 mål kan man nu gå vidare

--- Assets/Scripts/GoalBehaviour.cs
+++ Assets/Scripts/GoalBehaviour.cs
@@ -7,8 +7,11 @@ public class GoalBehaviour : MonoBehaviour
     [SerializeField] private TextMeshProUGUI amountOfScoresText;
     [SerializeField] private GameObject footballPrefab;
     [SerializeField] private Transform footballSpawnPos;
+    [SerializeField] private GameObject pathBlockerObj;
     
     int amountOfScores = 0;
+    
+    private bool finished = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -18,6 +21,13 @@ public class GoalBehaviour : MonoBehaviour
             amountOfScores++;
             
             amountOfScoresText.text = amountOfScores.ToString() + " scores / 3";
+
+            if (amountOfScores == 3 && !finished)
+            {
+                finished = true;
+                
+                pathBlockerObj.SetActive(false);
+            }
             
             Instantiate(footballPrefab, footballSpawnPos.position, Quaternion.identity);
         }
